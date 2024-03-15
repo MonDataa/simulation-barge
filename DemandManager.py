4254e2361002fdbe436690b144ff1e472eb3e0b1
Mounsifelkhiyar <moncefelkhiyar@gmail.com>
2024-03-15 18:50:37 +0100

    add sed barge

--- DemandManager.py
+++ DemandManager.py
@@ -3,6 +3,5 @@ class DemandManager:
         self.port_activities = port_activities
 
     def get_demands_for_port(self, port):
-        # Cloner la liste des demandes pour le port spécifié pour éviter la modification des données originales
         demands = self.port_activities.get(port, {}).get('demands', [])
-        return demands[:]  # Retourne une copie de la liste
+        return demands[:]
